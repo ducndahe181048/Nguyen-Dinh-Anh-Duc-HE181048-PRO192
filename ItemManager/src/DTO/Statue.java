d50dfce67db26845a9f83e20ee4c87361f32a763
ducndahe181048 <anhduck0212@gmail.com>
2023-10-06 14:03:32 +0700

    lab4

---
+++ ItemManager/src/DTO/Statue.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anhdu
+ */
+public class Statue extends Item{
+    private int weight;
+    private String colour;
+
+    public Statue() {
+    }
+
+    public Statue(int value, String creator,int weight, String colour) {
+        super(value, creator);
+        this.weight = weight;
+        this.colour = colour;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+    
+    public void output(){
+        System.out.println("Weight: " + this.weight);
+        System.out.println("Colour: " + this.colour);
+    }
+    
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter weight: ");
+        this.weight = sc.nextInt();
+        
+        System.out.println("Enter colour: ");
+        this.colour = sc.nextLine();
+    }
+}
